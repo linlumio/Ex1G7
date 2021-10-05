dcc67e1671eb0e813cef703115b3ee04c1ace2ae
Ale <akip-core@outlook.com>
2021-10-05 16:25:53 +0200

    implement landscape

--- app/src/main/java/com/example/ex1g7/MainActivity.kt
+++ app/src/main/java/com/example/ex1g7/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.ex1g7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setContentView(R.layout.table_layout)
     }
 }
